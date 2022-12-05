4061bc5c816492dbc67e059984cf415d0543dad0
cbozyap <cbozyap@gmail.com>
2022-12-05 23:35:02 +0300

    single line fix

--- Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py.py
+++ Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py.py
@@ -9,7 +9,7 @@ def head():
 @app.route('/mult')
 def number():
     var1, var2 = 15210, 38960
-    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
+return render_template('body.html', value1 = var1, value2 = var2, sum = var1+var2)
 
 
 if __name__ == '__main__':
